20f9d5f7d860723c1cf20c97aee6bc819c4c2301
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-02-05 16:43:14 -0600

    modified the content

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  // const markdownContent = `
+
+  //           npm -g -i pnpm
+  //           npm install -g pnpm
+  //           sudo npm install -g pnpm
+  //           npm install -g npm@11.1.0
+  //           pnpm install
+  //           curl -fsSL https://get.pnpm.io/install.sh | sh
+  //           export PATH="$HOME/.local/share/pnpm:$PATH"
+  //           source ~/.zshrc
+  //           pnpm install
+  //           npx create-zephyr-apps@latest
+  //           cd Development/nefher
+  //           cd Development/
+  //           rm -rf .git
+  //           git init
+  //           g add .
+  //           git commit -m "init commit"
+  //           git remote add origin [NAME_OF_YOUR_REPO]
+  //           g push
+  //           git push --set-upstream origin master\n
+  //           npm run build
+  //           rm -rf package-lock.json
+  //           pnpm install
+  //           npm run build
+  //           g add . && g commit -m "built the thinggy" && g push --force
+
+  //   `;
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      <h2>Anthony Gilbert's Blog ✍️</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* <p>
+          How to deploy my blog:
+        </p> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="card">
+        {/* <code>{markdownContent}
+        </code> */}
+        Welcome to my blog!  ( posts to come ...)
       </p>
     </>
   );
